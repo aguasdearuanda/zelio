94499e184087b6f2d8659ab4d05b02a5784a8df5
Davi Thiesse <davithss@gmail.com>
2018-12-08 19:38:33 -0300

    remove unnecessary files

--- db/migrate/20181118144422_add_new_column_to_admins.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddNewColumnToAdmins < ActiveRecord::Migration[5.2]
-  def change
-    add_column :admins, :first_name, :string
-    add_column :admins, :last_name, :string
-  end
-end
--- db/migrate/20181124141811_add_new_column_to_groups.rb
+++
@@ -1,6 +0,0 @@
-class AddNewColumnToGroups < ActiveRecord::Migration[5.2]
-  def change
-    add_column :groups, :created_at, :datetime
-    add_column :groups, :updated_at, :datetime
-  end
-end
