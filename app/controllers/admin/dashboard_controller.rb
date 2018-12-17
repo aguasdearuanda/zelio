684b4dbe9f0c3b8d0ec9a05e7c99c16f1e93f6a2
Davi Thiesse <davithss@gmail.com>
2018-12-17 18:19:48 -0300

    changed dashboard and attendances

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Admin::DashboardController < AdminController
-  def index; end
+  def index
+    @klasses = Klass.all
+    @students = Student.all
+    @attendances = Attendance.all
+  end
 end
