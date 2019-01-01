2e1d7df3836ed4f3568d16dd26f78a5118a84141
Davi Thiesse <davithss@gmail.com>
2019-01-01 19:25:57 -0300

    fix heroku attachment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: Rails.application.credentials.dig(:aws, :bucket_name),
-      access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      s3_region: 'us-east-1',
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
