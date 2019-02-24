301a1ac81753c3f48e138fa31dc8e20d72f8aac0
Davi Thiesse <davithss@gmail.com>
2019-02-24 18:26:04 -0300

    added new method to get an array with attendances classes

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,18 +8,18 @@ class Student < ApplicationRecord
   def count_presence(attendances, klass)
     count = 0
     count = if situations.where(klass_id: klass).present?
-              period = attendances.where("realized_at <= ?", self.situations.first.created_at)
-              period_attendances(attendances, period)
+              period = attendances.where('realized_at <= ?', situations.first.created_at)
+              period_attendances(period)
             else
-              period = attendances.where('realized_at >= ?', self.created_at)
-              period_attendances(attendances, period)
+              period = attendances.where('realized_at >= ?', created_at)
+              period_attendances(period)
             end
     count
     # count = (count.to_f/attendances.size.to_f*100)
     # convert_to_percentage(count)
   end
 
-  def period_attendances(attendances, period)
+  def period_attendances(period)
     # 1st scenario: the student was disabled so the next classes attendance,
     # should not be counted.
     # 2nd scenario: the student joined on the group days later after began
@@ -30,5 +30,22 @@ class Student < ApplicationRecord
       count += 1 if attended.students.where(id: id).empty?
     end
     count
+  end
+
+  def show_attendances(attendances, klass)
+    attendance = []
+    period = check_presence(attendances, klass)
+    period.each do |klasses|
+      attendance << klasses.name if klasses.students.where(id: id).empty?
+    end
+    attendance.join('<br>').html_safe
+  end
+
+  def check_presence(attendances, klass)
+    period = if situations.where(klass_id: klass).present?
+               attendances.where('realized_at <= ?', situations.first.created_at)
+             else
+               attendances.where('realized_at >= ?', created_at)
+             end
   end
 end
