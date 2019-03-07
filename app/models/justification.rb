381e4ad0d9a94f44501d7023e042cdb5cf5cf92f
Davi Thiesse <davithss@gmail.com>
2019-03-06 22:34:57 -0300

    hound fixes

--- app/models/justification.rb
+++ app/models/justification.rb
@@ -3,7 +3,7 @@
 class Justification < ApplicationRecord
   has_attached_file :upload
 
-  validates_attachment_content_type :upload, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :upload, content_type: %r{\Aimage\/.*\Z}
   validates :name, :date, :email, :description, presence: true
   enum status: {
     pendente: 'pendente',
