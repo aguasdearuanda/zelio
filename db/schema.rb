8ef5635a73373108c57f89903f1ad14600289005
Davi Thiesse <davithss@gmail.com>
2019-03-12 16:13:24 -0300

    rubocop changes in schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_218_183_539) do
+ActiveRecord::Schema.define(version: 2019_02_18_183539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
