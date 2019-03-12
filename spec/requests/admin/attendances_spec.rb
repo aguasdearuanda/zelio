4849fed6ef4879520eada5e4b249806003187d72
Davi Thiesse <davithss@gmail.com>
2019-03-12 16:11:51 -0300

    covering attendances controller

--- spec/requests/admin/attendances_spec.rb
+++ spec/requests/admin/attendances_spec.rb
@@ -19,6 +19,13 @@ RSpec.describe 'Attendances management', type: :request do
     end
   end
 
+  describe '#show' do
+    it 'shows a attendance' do
+      get admin_klass_attendance_path(@klass.first.id, @attendances.first.id)
+      expect(response).to be_successful
+    end
+  end
+
   describe '#new' do
     it 'creates a attendance' do
       get new_admin_klass_attendance_path(@klass.first.id)
@@ -28,11 +35,38 @@ RSpec.describe 'Attendances management', type: :request do
     end
   end
 
+  describe '#create' do
+    it 'creates a attendance' do
+      post admin_klass_attendances_path(klass_id: @klass.first.id),
+           params: { attendance: { name: 'aula1' } }
+      expect(response).to redirect_to(admin_klass_attendances_path)
+    end
+
+    it 'will not create a attendance' do
+      post admin_klass_attendances_path(klass_id: @klass.first.id),
+           params: { attendance: { name: '' } }
+      expect(response).to render_template :new
+    end
+  end
+
+  describe '#edit' do
+    it 'edit a attendance' do
+      get edit_admin_klass_attendance_path(@klass.first.id, @attendances.first.id)
+      expect(response).to be_successful
+    end
+  end
+
   describe '#update' do
-    let(:attendance) { create(:attendance, name: 'Aula 1') }
-    it 'changes attendance name' do
-      attendance.update(name: 'Aula 2')
-      expect(attendance.name).to eq('Aula 2')
+    it 'update a attendance with success' do
+      put admin_klass_attendance_path(klass_id: @klass.first.id, id: @attendances.first.id),
+          params: { attendance: { name: 'aula x' } }
+      expect(response).to redirect_to admin_klass_attendances_path(@klass.first.id)
+    end
+
+    it 'will redirect to edit page if something went wrong' do
+      put admin_klass_attendance_path(klass_id: @klass.first.id, id: @attendances.first.id),
+          params: { attendance: { name: '' } }
+      expect(response).to render_template :edit
     end
   end
 
