1e3394c21e9e0a3ffa33107703816cabd8f09f8f
Davi Thiesse <davithss@gmail.com>
2019-09-05 00:26:12 -0300

    refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,35 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin, path: 'a' do
+    authenticated :admin do
+      root 'dashboard#index'
+      get '/dashboard', to: 'dashboard#index', as: 'dashboard'
+      get '/a/sign_out', to: 'devise/sessions#destroy', as: :delete
+      resources :students
+      resources :klasses do
+        resources :attendances, controller: '/admin/attendances'
+        get 'disable_student', to: 'klasses#disable_student'
+      end
+      # resources :situations
+      resources :teachers
+      resources :justifications, only: %i[index show edit update]
+    end
+  end
+
+  namespace :student, path: 's' do
+    authenticated :student do
+      get '/dashboard', to: 'dashboard#index', as: 'dashboard'
+      resources :justifications, only: %i[index show new create]
+    end
+  end
+
+  devise_for :students, path: 's', controllers: { sessions: 'sessions',
+                                                  confirmations: 'student/confirmation' }
   devise_for :admins, path: 'a', controllers: { sessions: 'sessions' }
 
-  devise_scope :admin do
-    namespace :admin, path: 'a' do
-      authenticated :admin do
-        root 'dashboard#index'
-        get '/dashboard', to: 'dashboard#index', as: 'dashboard'
-        get '/a/sign_out', to: 'devise/sessions#destroy', as: :delete
-        resources :students
-        resources :klasses do
-          resources :attendances, controller: '/admin/attendances'
-          get 'disable_student', to: 'klasses#disable_student'
-        end
-        # resources :situations
-        resources :teachers
-        resources :justifications, only: %i[index show edit update]
-      end
-    end
+  root 'devise/sessions#new', as: :unauthenticated_root
 
-    root 'devise/sessions#new', as: :unauthenticated_root
-    resources :justifications, path: 'justificativa', path_names: { new: 'novo' }, only: %i[new create]
-  end
+  resources :justifications, path: 'justificativa', path_names: { new: 'novo' },
+                                                    only: %i[new create]
 end
