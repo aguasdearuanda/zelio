bb29f19092f7daa8b87466bf6b2818a37a00b87b
Davi Thiesse <davithss@gmail.com>
2019-09-05 00:20:31 -0300

    add devise on student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
   has_and_belongs_to_many :klasses
   has_and_belongs_to_many :attendances
   has_many :situations
