1ecf99bf86fd8c5cb0f636982a9fc56c4e23b020
Davi Thiesse <davithss@gmail.com>
2019-09-06 15:53:45 -0300

    add new actions to student justification controller

--- app/controllers/student/justifications_controller.rb
+++ app/controllers/student/justifications_controller.rb
@@ -8,4 +8,26 @@ class Student::JustificationsController < StudentController
   def show
     @justification = Justification.find(params[:id])
   end
+
+  def new
+    @justification = Justification.new
+    @klasses = Klass.all
+  end
+
+  def create
+    @justification = Justification.new(justifications_params)
+
+    flash[:notice] = if @justification.save
+                       'Justificativa enviada com sucesso!'
+                     else
+                       'Algo deu errado, tente novamente!'
+                     end
+    redirect_to student_justifications_path
+  end
+
+  private
+
+  def justifications_params
+    params.require(:justification).permit(:name, :date, :email, :description, :upload, :klass)
+  end
 end
