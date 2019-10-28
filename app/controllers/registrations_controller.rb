e7fe111e3f263260184d11037f3353a9e49a5781
Davi Thiesse <davithss@gmail.com>
2019-10-28 15:20:32 -0300

    change after_update method to use required parameter

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_update_path_for(_resource)
+  def after_update_path_for(resource)
     case resource
     when Admin
       admin_dashboard_path
