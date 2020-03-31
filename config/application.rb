a48c643127e37cad3b4cd527382dd3f72a24f79d
Davi Thiesse <davithss@gmail.com>
2020-03-31 00:49:39 -0300

    fix application config assets

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Zelio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    # config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.time_zone = 'America/Maceio'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
