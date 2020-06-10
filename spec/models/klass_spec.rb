b850917d5e5614f2353f49954d090a688298930a
Davi Thiesse <davithss@gmail.com>
2020-06-10 19:23:27 -0300

    create specs for the new method

--- spec/models/klass_spec.rb
+++ spec/models/klass_spec.rb
@@ -35,4 +35,25 @@ RSpec.describe Klass, type: :model do
       expect(student.situations).to be_any
     end
   end
+
+  describe '#active_students' do
+    before do
+      @klass = create(:klass)
+      @students = create_list(:student, 3, klass_ids: [@klass.id])
+    end
+
+
+    it 'check how many students in the class' do
+      expect(@klass.students.count).to be_equal(3)
+    end
+
+    context 'when disable a student' do
+      it 'show only active students' do
+        student = @students.first
+        expect(student.situations).not_to be_any
+        @klass.disable_student(student.id)
+        expect(@klass.active_students.count).to be_equal(2)
+      end
+    end
+  end
 end
